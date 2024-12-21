775780389aa8f44111f95b806bd916e8704dd949
dotAKP <akpatel6261@gmail.com>
2024-12-21 12:19:19 +0530

    Add Edit & Remove Task

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const inputBox = document.getElementById('inputBox');
 const addBtn = document.getElementById('addBtn');
 const todoList = document.getElementById('todoList');
 
+let editTodo = null;
+
+// Function to add Todo : create
 const addTodo = ()=>{
     const inputText = inputBox.value.trim();
     if(inputText.length <= 0){
@@ -9,26 +12,56 @@ const addTodo = ()=>{
         return false;
     }
 
-    // creating p tag
-    const li = document.createElement('li');
-    const p = document.createElement('p');
-    p.innerHTML = inputText;
-    li.appendChild(p);
+    if(addBtn.value === "Edit"){
+        editTodo.target.previousElementSibling.innerHTML = inputText;
+        addBtn.value = "Add";
+        inputBox.value = "";
+    }
+    else {
+        // creating p tag
+        const li = document.createElement('li');
+        const p = document.createElement('p');
+        p.innerHTML = inputText;
+        li.appendChild(p);
 
-    // creating Edit Btn
-    const editBtn = document.createElement('button');
-    editBtn.innerText = "Edit";
-    editBtn.classList.add("btn","editBtn");
-    li.appendChild(editBtn);
+        // creating Edit Btn
+        const editBtn = document.createElement('button');
+        editBtn.innerText = "Edit";
+        editBtn.classList.add("btn","editBtn");
+        li.appendChild(editBtn);
 
-    // creating delete Btn
-    const deleteBtn = document.createElement('button');
-    deleteBtn.innerText = "Remove";
-    deleteBtn.classList.add("btn","deleteBtn");
-    li.appendChild(deleteBtn);
+        // creating delete Btn
+        const deleteBtn = document.createElement('button');
+        deleteBtn.innerText = "Remove";
+        deleteBtn.classList.add("btn","deleteBtn");
+        li.appendChild(deleteBtn);
+    
 
-    todoList.appendChild(li);
-    inputBox.value = "";  
+        todoList.appendChild(li);
+        inputBox.value = "";  
+        
+    }
 }
 
+// Function to update Todo  : edit/remove
+const updateTodo = (e)=>{
+    // Remove Task
+    if(e.target.innerHTML === "Remove"){
+        todoList.removeChild(e.target.parentElement);
+    }
+    
+    // Edit Task
+    if(e.target.innerHTML === "Edit"){
+        inputBox.value = e.target.previousElementSibling.innerHTML;
+        inputBox.focus();
+        addBtn.value = "Edit";
+        editTodo = e;
+    }
+    
+
+}
+
+
+
 addBtn.addEventListener('click',addTodo);
+todoList.addEventListener('click',updateTodo);
